1f30e2913901fd1f8fad7356215e1c625686a780
Kenni95 <133218651+Kenni95@users.noreply.github.com>
2025-08-27 22:21:03 +0200

    first-commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import CreatePage from "./Pages/CreatePage";
+import HomePage from "./Pages/HomePage";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  
+  return (
+    <>
+      <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+        <Navbar />
+      <Routes>
+        <Route>
+         <Route path="/" element={<HomePage />} />
+         <Route path="/CreatePage" element={<CreatePage />} />
+        </Route>
+      </Routes>
+      </Box>
+    </>
+  );
+}
+
+export default App; 
